734e344dcf03b6d9296966bf3aef452f903689b3
greatden2007 <kudinov.dw@gmail.com>
2014-04-14 18:08:08 +0400

    v. 1.0 Include: editing SVG tags, Markers, changing Headers(h4, h3), correcting quotes.

--- Maker.py
+++ Maker.py
@@ -5,14 +5,13 @@ import zipfile
 from os import listdir
 from os.path import isfile, join
 import shutil
-from shutil import make_archive
-import zlib
+
 
 class Maker(object):
     def __init__(self):
         self.location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         self.onlyfiles = [f for f in listdir(self.location) if isfile(join(self.location, f))]
-        print("\t--- Found {} epubs:".format(len(self.onlyfiles) - 1))
+        print("\t--- Found {} files:".format(len(self.onlyfiles) - 1))
 
     def editEpub(self):
         i = 0
@@ -24,6 +23,7 @@ class Maker(object):
                 inZipPath = os.path.join(self.location, epubNameWithoutFormat)
                 outZipPath = os.path.join(self.location, epubnameWithFormat)
 
+                print('\t--- Current epub: {}'.format(epubnameWithFormat))
                 self.unarchiveEpub(epubnameWithFormat, epubNameWithoutFormat)
                 self.correctAllHtml(htmlFolder)
                 os.remove(outZipPath)
@@ -42,16 +42,26 @@ class Maker(object):
             z.extractall(outputPath)
 
     def correctAllHtml(self, htmlFolder):
-        allHtmls = [f for f in listdir(htmlFolder) if isfile(join(htmlFolder, f))]
+        try:
+            allHtmls = [f for f in listdir(htmlFolder) if isfile(join(htmlFolder, f))]
+        except:
+            print("First try finding HTML directory failed. Second try...")
+            htmlFolder = os.path.join(htmlFolder[:-10], "OEBPS")
+            allHtmls = [f for f in listdir(htmlFolder) if isfile(join(htmlFolder, f))]
         for htmlName in allHtmls:
-            html = os.path.join(htmlFolder, htmlName)
-            self.replace_svg_tags(html)
-            self.replace_markers(html)
+            print('\t--- Current file: {}'.format(htmlName))
+            try:
+                html = os.path.join(htmlFolder, htmlName)
+                self.replace_svg_tags(html)
+                self.replace_markers(html)
+                self.replace_headers(html)
+            except:
+                print("\t--- Skip file: {}".format(htmlName))
 
 
     def replace_svg_tags(self, filename):
-        print('\t--- Replasing SVG Tags...')
-        f = open(os.path.join(self.location, filename), 'r')
+        print('\t\t--- Replasing SVG Tags...')
+        f = open(os.path.join(self.location, filename), 'r', encoding='utf-8')
         pattern = r'<svg:.+?href="(.+?)".+?svg>'
         INPUT = f.read()
         strings = re.findall(pattern, INPUT, flags=re.S)
@@ -71,32 +81,60 @@ class Maker(object):
             i += 1
         #print(INPUT)
         f.close()
-        f = open(os.path.join(self.location, filename), 'w')
+        f = open(os.path.join(self.location, filename), 'w', encoding='utf-8')
         f.write(INPUT)
+        f.close()
 
     def replace_markers(self, filename):
-        print('\t--- Replasing Markers...')
-        f = open(os.path.join(self.location, filename), 'r')
+        print('\t\t--- Replasing Markers...')
+        f = open(os.path.join(self.location, filename), mode='r', encoding='utf-8')
         pattern = r'<p class=".+?><img.+?"\/>(.+?)<\/p>'
+        pattern_full_message = r'(<p class=".+?><img.+?"\/>.+?<\/p>)'
         INPUT = f.read()
-        strings = re.findall(pattern, INPUT, flags=re.S)
+        # print(INPUT)
+        full_messages = re.findall(pattern_full_message, INPUT)  # строки целиком вместе с тегами
+        strings = re.findall(pattern, INPUT)  # только текст
         i = 0
-        correctStrings = []
+        correctStrings = []  # исправленные строки с тегами
         for text in strings:
-            #print(text)
             correctStrings.append('<ul><li>{!s}</li></ul>'.format(text))
+            INPUT = INPUT.replace(full_messages[i], correctStrings[i])
+            i += 1
+            # print(INPUT)
+        print('\t\t--- Editing <ul> and <li> tags...')
+        INPUT = INPUT.replace("</ul>\n<ul>", "\n")
+        f.close()
+        f = open(os.path.join(self.location, filename), 'w', encoding='utf-8')
+        f.write(INPUT)
+        f.close()
 
-            originalString = INPUT
-            pattern = '(<p class=".+?><img.+?"\/>.+?<\/p>)'
-            pattern_obj = re.compile(pattern, re.S)
-            replacement_string = correctStrings[i]
-            INPUT = pattern_obj.sub(replacement_string, originalString, count=1)
+    def replace_headers(self, filename):
+        print('\t\t--- Replasing headers...')
+        f = open(os.path.join(self.location, filename), 'r', encoding='utf-8')
+        pattern = r'<h. class="h(.)".+?\/h.>'
+        pattern_full_message = r'(<h. class="h.".+?\/h.>)'
+        INPUT = f.read()
+        strings = re.findall(pattern, INPUT)  # только текст
+        full_message = re.findall(pattern_full_message, INPUT)
+        i = 0
+        INPUT = INPUT.replace("h4", "h3")
+        for text in full_message:
+            if int(strings[i]) == 2:
+                new_message = '<div class="chapter_header">{!s}</div>'.format(text)
+                INPUT = INPUT.replace(text, new_message)
+            # if int(strings[i]) == 3:
+            #     temp_string = strings[i].capitalize()
+            #     new_message = text.replace(strings[i], temp_string)
+            #     INPUT = INPUT.replace(text, new_message)
 
             i += 1
-        #print(INPUT)
+        print('\t\t--- Correcting quotes...')
+        INPUT = INPUT.replace("chap-bq", "right")
         f.close()
-        f = open(os.path.join(self.location, filename), 'w')
+        f = open(os.path.join(self.location, filename), 'w', encoding='utf-8')
         f.write(INPUT)
+        f.close()
+
 
     def zip(self, res, dst):
         zf = zipfile.ZipFile("%s" % (dst), "w", zipfile.ZIP_DEFLATED)
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import Maker
 import os
 
 maker = Maker.Maker()
-#uncomment following line to use in directory
-# maker.editEpub()
 
-filename = os.path.join(maker.location, "file.html")
-maker.replace_markers(filename)
+#uncomment following line to use in directory
+maker.editEpub()
+
+#uncomment following lines to use for only file
+# filename = os.path.join(maker.location, "bano_9781601636850_oeb_fm2_r1.html")
+# maker.replace_svg_tags(filename)
+# maker.replace_markers(filename)
+# maker.replace_headers(filename)
