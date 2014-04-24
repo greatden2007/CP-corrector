7ac0c6e25afdeb2fb5772a6b6d769315cddb31eb
greatden2007 <kudinov.dw@gmail.com>
2014-04-24 15:37:44 +0400

    v. 1.2 toc.ncx & .opf files correction (#)

--- Maker.py
+++ Maker.py
@@ -4,12 +4,22 @@ import re
 import zipfile
 from os import listdir
 from os.path import isfile, join
+from bs4 import BeautifulSoup
 import shutil
 
 
 class Maker(object):
     def __init__(self):
+        # non-safe defenition ( = "" ). Sets inside correctAllHtml()
         self.toc_folder = ""
+        self.html_folder = ""
+        self.epub_number = ""
+        # self.ok = True
+        self.toc_ids = []
+        self.opf_ids_part1 = []
+        self.opf_ids_part2 = []
+        self.big_strings = []
+        self.small_strings = []
         self.location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         self.onlyfiles = [f for f in listdir(self.location) if isfile(join(self.location, f))]
         print("\t--- Found {} files:".format(len(self.onlyfiles) - 1))
@@ -24,11 +34,17 @@ class Maker(object):
                 self.toc_folder = os.path.join(self.location, epubNameWithoutFormat + "/ops/")
                 inZipPath = os.path.join(self.location, epubNameWithoutFormat)
                 outZipPath = os.path.join(self.location, epubnameWithFormat)
+                self.epub_number = epubNameWithoutFormat
 
                 print('\t--- Current epub: {}'.format(epubnameWithFormat))
                 self.unarchiveEpub(epubnameWithFormat, epubNameWithoutFormat)
                 self.correctAllHtml(htmlFolder)
                 os.remove(outZipPath)
+                # if self.ok:
+                #     outZipPath = inZipPath + "_ok.epub"
+                # else:
+                #     outZipPath = inZipPath + "_not_ok.epub"
+                #     self.ok = True
                 self.zip(inZipPath, outZipPath)
                 shutil.rmtree(inZipPath)
                 i += 1
@@ -44,13 +60,15 @@ class Maker(object):
             z.extractall(outputPath)
 
     def correctAllHtml(self, htmlFolder):
-        self.repair_toc_ncx()
         try:
             allHtmls = [f for f in listdir(htmlFolder) if isfile(join(htmlFolder, f))]
+            self.html_folder = htmlFolder
         except:
             print("First try finding HTML directory failed. Second try...")
             htmlFolder = os.path.join(htmlFolder[:-10], "OEBPS")
+            self.html_folder = htmlFolder
             allHtmls = [f for f in listdir(htmlFolder) if isfile(join(htmlFolder, f))]
+        self.repair_toc_ncx()
         for htmlName in allHtmls:
             print('\t--- Current file: {}'.format(htmlName))
             try:
@@ -62,6 +80,137 @@ class Maker(object):
                 print("\t--- Skip file: {}".format(htmlName))
 
     def repair_toc_ncx(self):
+        print('\t\t--- Repairing toc.ncx file, .opf file...')
+        self.toc_folder = os.path.join(self.toc_folder, "toc.ncx")
+        try:
+            f = open(self.toc_folder, 'r', encoding='utf-8')
+        except:
+            print("\t\t\t--- ! toc.ncx not in ops directory")
+            self.toc_folder = os.path.join(self.toc_folder[:-11], "OEBPS/toc.ncx")
+            f = open(self.toc_folder, 'r', encoding='utf-8')
+        for file in os.listdir(self.toc_folder[:-7]):
+            if file.endswith(".opf"):
+                opf_file = os.path.join(self.toc_folder[:-7], file)
+
+        pattern = r'src="(.+?\..*html)#(.+?)"'
+        toc_id_pattern = r'id\s*=\s*"(.+?)"'
+        INPUT = f.read()
+
+        self.toc_ids = re.findall(toc_id_pattern, INPUT)
+        opfFile = open(opf_file, 'r', encoding='utf-8')
+        opfINPUT = opfFile.read()
+
+        strings = re.findall(pattern, INPUT)
+        i = 0
+        toc_id_pattern = r'<navPoint id="(.+?)".+?<content src="(.+?html)\#*(.*?)"'
+        old_ids_refs_parts = re.findall(toc_id_pattern, INPUT, flags=re.S)
+
+        for chapter in strings:
+            # self.ok = False
+            filename = chapter[0]
+            part = chapter[1]
+            self.splitHTML(filename, part)
+            new_name = self.change_file_name(filename, part)
+            old_str = filename + "#" + part
+            INPUT = INPUT.replace(old_str, new_name, 1)
+            # old_ids_refs_parts[self.toc_ids[i]] = filename
+            # new_ids_refs_parts[self.toc_ids[i]] = new_name
+            i += 1
+
+        toc_id_pattern = r'<navPoint id="(.+?)".+?<content src="(.+?html)"'
+        new_ids_refs_parts = re.findall(toc_id_pattern, INPUT, flags=re.S)
+
+        f.close()
+        f = open(self.toc_folder, 'w', encoding='utf-8')
+        f.write(INPUT)
+        f.close()
+
+
+        #работа с 1/2 блоками в .opf + удаление информации из toc.ncx и .opf
+        ids = []
+        new_lines = ""
+        pattern = r'(<item id=".+?" href="(.+?)" media-type=".+?" \/>)'
+        lines = []
+        f = open(self.toc_folder, 'r', encoding='utf-8')
+        INPUT = f.read()
+        # part 1
+        old_lines = re.findall(pattern, opfINPUT)
+        for old_line in old_lines:
+            for id_ref_part in old_ids_refs_parts:
+                if id_ref_part[1] == old_line[1]:
+                    for new_id_ref_part in new_ids_refs_parts:
+                        if id_ref_part[0] == new_id_ref_part[0]:
+                            #создаем новую строку и добавляем в массив новых строк
+                            string = '<item id="{}" href="{}" media-type="application/xhtml+xml" />'.format(
+                                id_ref_part[0], new_id_ref_part[1])
+                            new_lines += string + "\n"
+                            ids.append(new_id_ref_part[0])
+                            lines.append(string)
+            if "" != new_lines:
+                if len(lines) > 1:
+                    lines = []
+                    opfINPUT = opfINPUT.replace(old_line[0], new_lines)
+                    opfINPUT = opfINPUT.replace(old_line[0], "")
+                    new_lines = ""
+                    ids_string = ""
+                    # part 2
+                    for part_id in ids:
+                        ids_string += '<itemref idref="{}" />\n'.format(part_id)
+                    opfINPUT = opfINPUT.replace('<itemref idref="{}" />'.format(ids[0]), ids_string)
+                    opfINPUT = opfINPUT.replace('<itemref idref="{}" />'.format(ids[0]), "")
+                    #удаление из toc.ncx
+                    toc_delete_pattern = '(<navPoint id="{}".+?<content src=".+?html"\s*\/>)'.format(ids[0])
+                    ids = []
+                    replacement = re.findall(toc_delete_pattern, INPUT, flags=re.S)
+                    INPUT = INPUT.replace(replacement[0], "")
+                new_lines = ""
+                lines = []
+                ids = []
+        f.close()
+        f = open(self.toc_folder, 'w', encoding='utf-8')
+        soup = BeautifulSoup(INPUT)
+        INPUT = soup.prettify()
+        f.write(INPUT)
+        f.close()
+
+        opfFile.close()
+        opfFile = open(opf_file, 'w', encoding='utf-8')
+        opfFile.write(opfINPUT)
+        opfFile.close()
+
+
+
+    def splitHTML(self, filename, splitter):
+        f = open(os.path.join(self.html_folder, filename), 'r', encoding='utf-8')
+        pattern = r'<\w+?><a id="(.+?)"\s*\/>\s*<\/\w+>(.+?)(?=<\w+?><a id=".+?"\s*\/>\s*<\/\w+>|<\/body>)'
+        head_pattern = r'(.+?)<body>'
+
+        INPUT = f.read()
+        head = re.match(head_pattern, INPUT, flags=re.S)
+        head = head.group(0)
+        contents = re.findall(pattern, INPUT, flags=re.S)
+        for content_group in contents:
+            if splitter == content_group[0]:
+                content = content_group[1]
+
+        end = "</body>\n</html>"
+        OUTPUT = head + content + end
+        f.close()
+        dot_splitter = "."
+        file = filename.split(dot_splitter)
+        new_file = file[0] + splitter + "." + file[1]
+        f = open(os.path.join(self.html_folder, new_file), 'w', encoding='utf-8')
+        f.write(OUTPUT)
+        f.close()
+
+    def change_file_name(self, old_name, chapter_name):
+        dot_splitter = "."
+        old_format = old_name.split(dot_splitter)[1]
+        old_name = old_name.split(dot_splitter)[0]
+        new_name = old_name + chapter_name + "." + old_format
+        return new_name
+
+    def repair_toc_ncx_NOT_USED(self):
         print('\t\t--- Repairing toc.ncx file...')
         self.toc_folder = os.path.join(self.toc_folder, "toc.ncx")
         try:
@@ -156,7 +305,6 @@ class Maker(object):
         f = open(os.path.join(self.location, filename), 'w', encoding='utf-8')
         f.write(INPUT)
         f.close()
-
 
     def zip(self, res, dst):
         zf = zipfile.ZipFile("%s" % (dst), "w", zipfile.ZIP_DEFLATED)
