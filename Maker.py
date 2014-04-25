51426b7645baf2f34d448cd9dd7a4e7c99b14b04
greatden2007 <kudinov.dw@gmail.com>
2014-04-25 18:49:16 +0400

    v. 1.3 good corrections still got problems with table of contents in the start of book and some html aren't parsed.

--- Maker.py
+++ Maker.py
@@ -69,6 +69,7 @@ class Maker(object):
             self.html_folder = htmlFolder
             allHtmls = [f for f in listdir(htmlFolder) if isfile(join(htmlFolder, f))]
         self.repair_toc_ncx()
+        # self.repair_toc_ncx_NOT_USED()
         for htmlName in allHtmls:
             print('\t--- Current file: {}'.format(htmlName))
             try:
@@ -92,7 +93,7 @@ class Maker(object):
             if file.endswith(".opf"):
                 opf_file = os.path.join(self.toc_folder[:-7], file)
 
-        pattern = r'src="(.+?\..*html)#(.+?)"'
+        pattern = r'src="(.*?\/)*(.+?\..*html)#(.+?)"'
         toc_id_pattern = r'id\s*=\s*"(.+?)"'
         INPUT = f.read()
 
@@ -102,13 +103,13 @@ class Maker(object):
 
         strings = re.findall(pattern, INPUT)
         i = 0
-        toc_id_pattern = r'<navPoint id="(.+?)".+?<content src="(.+?html)\#*(.*?)"'
+        toc_id_pattern = r'<navPoint id="(.+?)".+?<content src="(?:xhtml\/|)(.+?html)\#*(.*?)"'
         old_ids_refs_parts = re.findall(toc_id_pattern, INPUT, flags=re.S)
 
         for chapter in strings:
             # self.ok = False
-            filename = chapter[0]
-            part = chapter[1]
+            filename = chapter[1]
+            part = chapter[2]
             self.splitHTML(filename, part)
             new_name = self.change_file_name(filename, part)
             old_str = filename + "#" + part
@@ -117,7 +118,7 @@ class Maker(object):
             # new_ids_refs_parts[self.toc_ids[i]] = new_name
             i += 1
 
-        toc_id_pattern = r'<navPoint id="(.+?)".+?<content src="(.+?html)"'
+        toc_id_pattern = r'<navPoint id="(.+?)".+?<content src="(xhtml\/|)(.+?html)"'
         new_ids_refs_parts = re.findall(toc_id_pattern, INPUT, flags=re.S)
 
         f.close()
@@ -129,23 +130,25 @@ class Maker(object):
         #работа с 1/2 блоками в .opf + удаление информации из toc.ncx и .opf
         ids = []
         new_lines = ""
-        pattern = r'(<item id=".+?" href="(.+?)" media-type=".+?" \/>)'
+        pattern = r'(<item id="(.+?)" href="(?:xhtml\/|)(.+?)" media-type=".+?"\s*\/>)'
         lines = []
         f = open(self.toc_folder, 'r', encoding='utf-8')
         INPUT = f.read()
+        j = 0
         # part 1
         old_lines = re.findall(pattern, opfINPUT)
         for old_line in old_lines:
             for id_ref_part in old_ids_refs_parts:
-                if id_ref_part[1] == old_line[1]:
+                if id_ref_part[1] == old_line[2]:
                     for new_id_ref_part in new_ids_refs_parts:
                         if id_ref_part[0] == new_id_ref_part[0]:
                             #создаем новую строку и добавляем в массив новых строк
                             string = '<item id="{}" href="{}" media-type="application/xhtml+xml" />'.format(
-                                id_ref_part[0], new_id_ref_part[1])
+                                id_ref_part[0], new_id_ref_part[2])
                             new_lines += string + "\n"
                             ids.append(new_id_ref_part[0])
                             lines.append(string)
+                            main_id = old_line[1]
             if "" != new_lines:
                 if len(lines) > 1:
                     lines = []
@@ -156,20 +159,25 @@ class Maker(object):
                     # part 2
                     for part_id in ids:
                         ids_string += '<itemref idref="{}" />\n'.format(part_id)
-                    opfINPUT = opfINPUT.replace('<itemref idref="{}" />'.format(ids[0]), ids_string)
-                    opfINPUT = opfINPUT.replace('<itemref idref="{}" />'.format(ids[0]), "")
+                    opfINPUT = opfINPUT.replace('<itemref idref="{}" />'.format(main_id), ids_string)
+                    opfINPUT = opfINPUT.replace('<itemref idref="{}" />'.format(main_id), "")
+                    opfINPUT = opfINPUT.replace('<itemref idref="{}"/>'.format(main_id), ids_string)
+                    opfINPUT = opfINPUT.replace('<itemref idref="{}"/>'.format(main_id), "")
                     #удаление из toc.ncx
-                    toc_delete_pattern = '(<navPoint id="{}".+?<content src=".+?html"\s*\/>)'.format(ids[0])
+                    toc_delete_pattern = '(<navPoint id="{}".+?<content src="(?:xhtml\/|).+?html"\s*\/>)'.format(ids[0])
                     ids = []
                     replacement = re.findall(toc_delete_pattern, INPUT, flags=re.S)
-                    INPUT = INPUT.replace(replacement[0], "")
+                    INPUT = INPUT.replace(replacement[0],
+                                          '<navPoint id="test{}" playOrder="1">\n<navLabel>\n<text>Praise</text>\n</navLabel>\n<content src="bano_9781601638687_oeb_fm1_r1.html"/>'.format(
+                                              j))
+                    j += 1
                 new_lines = ""
                 lines = []
                 ids = []
         f.close()
         f = open(self.toc_folder, 'w', encoding='utf-8')
-        soup = BeautifulSoup(INPUT)
-        INPUT = soup.prettify()
+        # soup = BeautifulSoup(INPUT)
+        # INPUT = soup.prettify()
         f.write(INPUT)
         f.close()
 
@@ -179,20 +187,23 @@ class Maker(object):
         opfFile.close()
 
 
-
     def splitHTML(self, filename, splitter):
         f = open(os.path.join(self.html_folder, filename), 'r', encoding='utf-8')
         pattern = r'<\w+?><a id="(.+?)"\s*\/>\s*<\/\w+>(.+?)(?=<\w+?><a id=".+?"\s*\/>\s*<\/\w+>|<\/body>)'
         head_pattern = r'(.+?)<body>'
-
+        content = ""
         INPUT = f.read()
         head = re.match(head_pattern, INPUT, flags=re.S)
         head = head.group(0)
         contents = re.findall(pattern, INPUT, flags=re.S)
+        if len(contents) == 0:
+            pattern = r'<a id="(.+?)".+?\/a>(.+?)(?=<a id=".+?".+?\/a>|<\/body>)'
+            contents = re.findall(pattern, INPUT, flags=re.S)
         for content_group in contents:
             if splitter == content_group[0]:
                 content = content_group[1]
-
+        if len(content) < 2:
+            return
         end = "</body>\n</html>"
         OUTPUT = head + content + end
         f.close()
@@ -213,12 +224,12 @@ class Maker(object):
     def repair_toc_ncx_NOT_USED(self):
         print('\t\t--- Repairing toc.ncx file...')
         self.toc_folder = os.path.join(self.toc_folder, "toc.ncx")
-        try:
-            f = open(self.toc_folder, 'r', encoding='utf-8')
-        except:
-            print("\t\t--- ! toc.ncx not in ops directory")
-            self.toc_folder = os.path.join(self.toc_folder[:-11], "OEBPS/toc.ncx")
-            f = open(self.toc_folder, 'r', encoding='utf-8')
+        # try:
+        f = open(self.toc_folder, 'r', encoding='utf-8')
+        # except:
+        #     print("\t\t--- ! toc.ncx not in ops directory")
+        #     self.toc_folder = os.path.join(self.toc_folder[:-11], "OEBPS/toc.ncx")
+        #     f = open(self.toc_folder, 'r', encoding='utf-8')
         pattern = r'html(#.+?)"'
         INPUT = f.read()
         strings = re.findall(pattern, INPUT, flags=re.S)
@@ -232,8 +243,19 @@ class Maker(object):
     def replace_svg_tags(self, filename):
         print('\t\t--- Replasing SVG Tags...')
         f = open(os.path.join(self.location, filename), 'r', encoding='utf-8')
-        pattern = r'<svg:.+?href="(.+?)".+?svg>'
+        splitter = "."
+        name = filename.split(splitter)[0]
         INPUT = f.read()
+        if "cover" in filename:
+            pattern = r'(<body.+?<\/p>)'
+            image_pattern = r'<body.+?src="(.+?)".+?<\/p>'
+            image = re.findall(image_pattern, INPUT, flags=re.S)
+            image = image[0]
+            new_string = '<body><div class="cover"><img alt="cover" height="95%" src="{}"/></div>'.format(image)
+            image_block = re.findall(pattern, INPUT, flags=re.S)
+            image_block = image_block[0]
+            INPUT = INPUT.replace(image_block, new_string)
+        pattern = r'<svg:.+?href="(.+?)".+?svg>'
         strings = re.findall(pattern, INPUT, flags=re.S)
         i = 0
         correctStrings = []
@@ -279,32 +301,36 @@ class Maker(object):
         f.close()
 
     def replace_headers(self, filename):
+        splitter = "."
+        format = filename.split(splitter)
+        format = format[1]
         print('\t\t--- Replasing headers...')
-        f = open(os.path.join(self.location, filename), 'r', encoding='utf-8')
-        pattern = r'<h. class="h(.)".+?\/h.>'
-        pattern_full_message = r'(<h. class="h.".+?\/h.>)'
-        INPUT = f.read()
-        strings = re.findall(pattern, INPUT)  # только текст
-        full_message = re.findall(pattern_full_message, INPUT)
-        i = 0
-        INPUT = INPUT.replace("h4", "h3")
-        for text in full_message:
-            if int(strings[i]) == 2:
-                new_message = '<div class="chapter_header">{!s}</div>'.format(text)
-                INPUT = INPUT.replace(text, new_message)
-            # if int(strings[i]) == 3:
-            #     temp_string = strings[i].capitalize()
-            #     new_message = text.replace(strings[i], temp_string)
-            #     INPUT = INPUT.replace(text, new_message)
+        if not "ncx" in filename and not "opf" in filename:
+            f = open(os.path.join(self.location, filename), 'r', encoding='utf-8')
+            pattern = r'<h. class="h(.)".+?\/h.>'
+            pattern_full_message = r'(<h. class="h.".+?\/h.>)'
+            INPUT = f.read()
+            strings = re.findall(pattern, INPUT)  # только текст
+            full_message = re.findall(pattern_full_message, INPUT)
+            i = 0
+            INPUT = INPUT.replace("h4", "h3")
+            for text in full_message:
+                if int(strings[i]) == 2:
+                    new_message = '<div class="chapter_header">{!s}</div>'.format(text)
+                    INPUT = INPUT.replace(text, new_message)
+                # if int(strings[i]) == 3:
+                #     temp_string = strings[i].capitalize()
+                #     new_message = text.replace(strings[i], temp_string)
+                #     INPUT = INPUT.replace(text, new_message)
 
-            i += 1
-        print('\t\t--- Correcting quotes...')
-        INPUT = INPUT.replace("chap-bq", "right")
-        INPUT = INPUT.replace("&#8212;", " &#8212 ")
-        f.close()
-        f = open(os.path.join(self.location, filename), 'w', encoding='utf-8')
-        f.write(INPUT)
-        f.close()
+                i += 1
+            print('\t\t--- Correcting quotes...')
+            INPUT = INPUT.replace("chap-bq", "right")
+            INPUT = INPUT.replace("&#8212;", " &#8212 ")
+            f.close()
+            f = open(os.path.join(self.location, filename), 'w', encoding='utf-8')
+            f.write(INPUT)
+            f.close()
 
     def zip(self, res, dst):
         zf = zipfile.ZipFile("%s" % (dst), "w", zipfile.ZIP_DEFLATED)
